e1179c02f853836f2fbabd0b7ba585f1cc5df273
bgrhnzcn <bugrahanozcan16@gmail.com>
2024-11-10 02:45:02 +0300

    Refactor gl42.hpp: Update window dimensions based on user

--- inc/gl42.hpp
+++ inc/gl42.hpp
@@ -6,11 +6,22 @@
 /*   By: bgrhnzcn <bgrhnzcn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 13:46:26 by bgrhnzcn          #+#    #+#             */
-/*   Updated: 2024/10/15 01:30:34 by bgrhnzcn         ###   ########.fr       */
+/*   Updated: 2024/11/10 02:23:49 by bgrhnzcn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+#define USER_BUOZCAN 1
+#define USER_BGRHNZCN 2
+
+#if USER == USER_BUOZCAN
+#	define WIDTH 1800
+#	define HEIGHT 1600
+#else
+#	define WIDTH 800
+#	define HEIGHT 600
+#endif
 
 //#include "OpenGLDecl.hpp"
 #include "Window.hpp"
@@ -19,6 +30,7 @@
 #include "Vector2.hpp"
 #include "Vector3.hpp"
 #include "Vector4.hpp"
+#include "Vertex.hpp"
 #include "Matrix2x2.hpp"
 #include "VertexBuffer.hpp"
 #include "VertexArray.hpp"
