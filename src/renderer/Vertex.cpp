b3869a6dd0d5988abffd9fd086fa55fc01224741
bgrhnzcn <bugrahanozcan16@gmail.com>
2024-11-14 02:31:47 +0300

    Refactor Vertex class: Update variable names and constructor calls, add print function

--- src/renderer/Vertex.cpp
+++ src/renderer/Vertex.cpp
@@ -6,9 +6,11 @@
 /*   By: bgrhnzcn <bgrhnzcn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 02:15:34 by bgrhnzcn          #+#    #+#             */
-/*   Updated: 2024/11/10 02:27:57 by bgrhnzcn         ###   ########.fr       */
+/*   Updated: 2024/11/13 21:54:28 by bgrhnzcn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iostream>
 
 #include "Vertex.hpp"
 
@@ -17,10 +19,39 @@ gl42::Vertex::Vertex()
 	//Do Nothing.
 }
 
-gl42::Vertex::Vertex(Vector3 position, Vector3 normal, Vector2 uv, Vector4 color)
+gl42::Vertex::Vertex(Vector3 position, Vector3 normal, Vector2 texture, Vector4 color)
 {
 	this->position = position;
 	this->normal = normal;
-	this->uv = uv;
+	this->texture = texture;
 	this->color = color;
+}
+
+void gl42::Vertex::print()
+{
+	std::cout << "Position: {"
+			  << position.x << ", "
+			  << position.y << ", "
+			  << position.z << "}" << std::endl;
+	std::cout << "Textures: {"
+			  << texture.x << ", "
+			  << texture.y << "}" << std::endl;
+	std::cout << "Normal: {"
+			  << normal.x << ", "
+			  << normal.y << ", "
+			  << normal.z << "}" << std::endl;
+}
+
+void gl42::Face::print()
+{
+	std::cout << "Face:\n" << "\tPositionInd: ";
+	for (int index : positionIndices)
+		std::cout << index << " ";
+	std::cout << std::endl << "\tNormalInd: ";
+	for (int index : normalIndices)
+		std::cout << index << " ";
+	std::cout << std::endl << "\tTextureInd: ";
+	for (int index : textureIndices)
+		std::cout << index << " ";
+	std::cout << std::endl;
 }
--- src/renderer/Vertex.hpp
+++ src/renderer/Vertex.hpp
@@ -6,11 +6,13 @@
 /*   By: bgrhnzcn <bgrhnzcn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 02:11:00 by bgrhnzcn          #+#    #+#             */
-/*   Updated: 2024/11/10 02:44:30 by bgrhnzcn         ###   ########.fr       */
+/*   Updated: 2024/11/13 23:23:45 by bgrhnzcn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+#include <vector>
 
 #include "Vector2.hpp"
 #include "Vector3.hpp"
@@ -21,12 +23,24 @@ namespace gl42
 	class Vertex
 	{
 		public:
+			Vertex();
+			Vertex(Vector3 position, Vector3 normal, Vector2 texture, Vector4 color);
+		public:
+			void print();
+		public:
 			Vector3 position;
 			Vector3 normal;
-			Vector2 uv;
-			Vector4 color;
+			Vector2 texture;
+			Vector4 color = 0;
+	};
+
+	class Face
+	{
 		public:
-			Vertex();
-			Vertex(Vector3 position, Vector3 normal, Vector2 uv, Vector4 color);
+			void print();
+		public:
+			std::vector<int> positionIndices;
+			std::vector<int> normalIndices;
+			std::vector<int> textureIndices;
 	};
 } // namespace gl42
